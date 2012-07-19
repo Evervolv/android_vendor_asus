45211c857281f52b36db0e80f75162127e3680af
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-07-19 00:08:01 -0500

    grouper: initial import from JRO03D

---
+++ grouper/device-vendor-blobs.mk
@@ -0,0 +1,95 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file was generated from device/asus/grouper/proprietary-blobs.txt
+
+PRODUCT_COPY_FILES += \
+    vendor/asus/grouper/proprietary/btmacreader:system/bin/btmacreader \
+    vendor/asus/grouper/proprietary/glgps:system/bin/glgps \
+    vendor/asus/grouper/proprietary/sensors-config:system/bin/sensors-config \
+    vendor/asus/grouper/proprietary/tf_daemon:system/bin/tf_daemon \
+    vendor/asus/grouper/proprietary/asound.conf:system/etc/asound.conf \
+    vendor/asus/grouper/proprietary/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
+    vendor/asus/grouper/proprietary/nvavp_os_00001000.bin:system/etc/firmware/nvavp_os_00001000.bin \
+    vendor/asus/grouper/proprietary/nvavp_os_0ff00000.bin:system/etc/firmware/nvavp_os_0ff00000.bin \
+    vendor/asus/grouper/proprietary/nvavp_os_e0000000.bin:system/etc/firmware/nvavp_os_e0000000.bin \
+    vendor/asus/grouper/proprietary/nvavp_os_eff00000.bin:system/etc/firmware/nvavp_os_eff00000.bin \
+    vendor/asus/grouper/proprietary/nvavp_vid_ucode_alt.bin:system/etc/firmware/nvavp_vid_ucode_alt.bin \
+    vendor/asus/grouper/proprietary/touch_fw.ekt:system/etc/firmware/touch_fw.ekt \
+    vendor/asus/grouper/proprietary/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
+    vendor/asus/grouper/proprietary/nvcamera.conf:system/etc/nvcamera.conf \
+    vendor/asus/grouper/proprietary/nvram.txt:system/etc/nvram.txt \
+    vendor/asus/grouper/proprietary/libEGL_tegra.so:system/lib/egl/libEGL_tegra.so \
+    vendor/asus/grouper/proprietary/libGLESv1_CM_tegra.so:system/lib/egl/libGLESv1_CM_tegra.so \
+    vendor/asus/grouper/proprietary/libGLESv2_tegra.so:system/lib/egl/libGLESv2_tegra.so \
+    vendor/asus/grouper/proprietary/camera.tegra3.so:system/lib/hw/camera.tegra3.so \
+    vendor/asus/grouper/proprietary/gps.tegra3.so:system/lib/hw/gps.tegra3.so \
+    vendor/asus/grouper/proprietary/gralloc.tegra3.so:system/lib/hw/gralloc.tegra3.so \
+    vendor/asus/grouper/proprietary/hwcomposer.tegra3.so:system/lib/hw/hwcomposer.tegra3.so \
+    vendor/asus/grouper/proprietary/keystore.grouper.so:system/lib/hw/keystore.grouper.so \
+    vendor/asus/grouper/proprietary/sensors.grouper.so:system/lib/hw/sensors.grouper.so \
+    vendor/asus/grouper/proprietary/libardrv_dynamic.so:system/lib/libardrv_dynamic.so \
+    vendor/asus/grouper/proprietary/libcgdrv.so:system/lib/libcgdrv.so \
+    vendor/asus/grouper/proprietary/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+    vendor/asus/grouper/proprietary/libinvensense_hal.so:system/lib/libinvensense_hal.so \
+    vendor/asus/grouper/proprietary/libmllite.so:system/lib/libmllite.so \
+    vendor/asus/grouper/proprietary/libmplmpu.so:system/lib/libmplmpu.so \
+    vendor/asus/grouper/proprietary/libnvapputil.so:system/lib/libnvapputil.so \
+    vendor/asus/grouper/proprietary/libnvasfparserhal.so:system/lib/libnvasfparserhal.so \
+    vendor/asus/grouper/proprietary/libnvaviparserhal.so:system/lib/libnvaviparserhal.so \
+    vendor/asus/grouper/proprietary/libnvavp.so:system/lib/libnvavp.so \
+    vendor/asus/grouper/proprietary/libnvcamerahdr.so:system/lib/libnvcamerahdr.so \
+    vendor/asus/grouper/proprietary/libnvddk_2d.so:system/lib/libnvddk_2d.so \
+    vendor/asus/grouper/proprietary/libnvddk_2d_v2.so:system/lib/libnvddk_2d_v2.so \
+    vendor/asus/grouper/proprietary/libnvdispmgr_d.so:system/lib/libnvdispmgr_d.so \
+    vendor/asus/grouper/proprietary/libnvmm.so:system/lib/libnvmm.so \
+    vendor/asus/grouper/proprietary/libnvmmlite.so:system/lib/libnvmmlite.so \
+    vendor/asus/grouper/proprietary/libnvmmlite_audio.so:system/lib/libnvmmlite_audio.so \
+    vendor/asus/grouper/proprietary/libnvmmlite_image.so:system/lib/libnvmmlite_image.so \
+    vendor/asus/grouper/proprietary/libnvmmlite_utils.so:system/lib/libnvmmlite_utils.so \
+    vendor/asus/grouper/proprietary/libnvmmlite_video.so:system/lib/libnvmmlite_video.so \
+    vendor/asus/grouper/proprietary/libnvmm_audio.so:system/lib/libnvmm_audio.so \
+    vendor/asus/grouper/proprietary/libnvmm_camera.so:system/lib/libnvmm_camera.so \
+    vendor/asus/grouper/proprietary/libnvmm_contentpipe.so:system/lib/libnvmm_contentpipe.so \
+    vendor/asus/grouper/proprietary/libnvmm_image.so:system/lib/libnvmm_image.so \
+    vendor/asus/grouper/proprietary/libnvmm_manager.so:system/lib/libnvmm_manager.so \
+    vendor/asus/grouper/proprietary/libnvmm_misc.so:system/lib/libnvmm_misc.so \
+    vendor/asus/grouper/proprietary/libnvmm_parser.so:system/lib/libnvmm_parser.so \
+    vendor/asus/grouper/proprietary/libnvmm_service.so:system/lib/libnvmm_service.so \
+    vendor/asus/grouper/proprietary/libnvmm_utils.so:system/lib/libnvmm_utils.so \
+    vendor/asus/grouper/proprietary/libnvmm_video.so:system/lib/libnvmm_video.so \
+    vendor/asus/grouper/proprietary/libnvmm_writer.so:system/lib/libnvmm_writer.so \
+    vendor/asus/grouper/proprietary/libnvodm_dtvtuner.so:system/lib/libnvodm_dtvtuner.so \
+    vendor/asus/grouper/proprietary/libnvodm_hdmi.so:system/lib/libnvodm_hdmi.so \
+    vendor/asus/grouper/proprietary/libnvodm_imager.so:system/lib/libnvodm_imager.so \
+    vendor/asus/grouper/proprietary/libnvodm_misc.so:system/lib/libnvodm_misc.so \
+    vendor/asus/grouper/proprietary/libnvodm_query.so:system/lib/libnvodm_query.so \
+    vendor/asus/grouper/proprietary/libnvomx.so:system/lib/libnvomx.so \
+    vendor/asus/grouper/proprietary/libnvomxilclient.so:system/lib/libnvomxilclient.so \
+    vendor/asus/grouper/proprietary/libnvos.so:system/lib/libnvos.so \
+    vendor/asus/grouper/proprietary/libnvparser.so:system/lib/libnvparser.so \
+    vendor/asus/grouper/proprietary/libnvrm.so:system/lib/libnvrm.so \
+    vendor/asus/grouper/proprietary/libnvrm_graphics.so:system/lib/libnvrm_graphics.so \
+    vendor/asus/grouper/proprietary/libnvsm.so:system/lib/libnvsm.so \
+    vendor/asus/grouper/proprietary/libnvtvmr.so:system/lib/libnvtvmr.so \
+    vendor/asus/grouper/proprietary/libnvwinsys.so:system/lib/libnvwinsys.so \
+    vendor/asus/grouper/proprietary/libnvwsi.so:system/lib/libnvwsi.so \
+    vendor/asus/grouper/proprietary/libsensors.lightsensor.so:system/lib/libsensors.lightsensor.so \
+    vendor/asus/grouper/proprietary/libstagefrighthw.so:system/lib/libstagefrighthw.so \
+    vendor/asus/grouper/proprietary/libtf_crypto_sst.so:system/lib/libtf_crypto_sst.so \
+    vendor/asus/grouper/proprietary/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
+    vendor/asus/grouper/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/asus/grouper/proprietary/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    vendor/asus/grouper/proprietary/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/asus/grouper/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so
